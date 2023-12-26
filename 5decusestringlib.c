85fea2722bf73c79d52b84dccebfca03b55dc64f
Shivesh kumar <152985427+shivesh005@users.noreply.github.com>
2023-12-26 14:56:56 +0530

    Add files via upload

---
+++ 5decusestringlib.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char A[]="shivesh";
+    char B[]="shivam";
+    char *strcat;
+
+    printf("%d\n",strcmp(A,B));
+    //printf("%d\n",strcpy(B,A));
+    //printf("%s\n",strcat(B,A));
+    //printf("after reversing %s",strrev(A));
+
+return 0;
+}
